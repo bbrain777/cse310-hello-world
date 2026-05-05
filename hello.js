3563bf7653c0d08bc34c9b39963fe5edd669e4a8
Olakunle Obademi <oobademi@byupathway.edu>
2026-05-05 11:48:52 +0100

    Initial commit

---
+++ hello.js
@@ -0,0 +1,6 @@
+// CSE 310 Hello World Program
+// Author: Olakunle Obademi
+
+console.log("Hello World!");
+console.log("My name is Olakunle Obademi.");
+console.log("This is my first CSE 310 JavaScript project.");
